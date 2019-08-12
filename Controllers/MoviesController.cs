1ca5ef7ad4f548a9241137e07164772d9282656e
PeerRoth <peterrodinis@gmail.com>
2019-08-12 03:15:30 -0500

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+//using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shreik!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customerr 2" } };
+
+        //    var viewModel = new RandomoMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+
+        //    };
+
+        //    return View( movie );
+        //    //return View(movie);
+        //    //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        //}
+        public ActionResult Edit( int id )
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult ByReleaseDate( int year, int month )
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
